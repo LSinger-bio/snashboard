50ca993e8815d316dfb0ff8c80d19ae9e6f3b3bb
LSinger-bio <lsinger9@jh.edu>
2024-12-02 20:59:34 -0500

    changed inat card to have tabs, added abundance plot and table, changed map to plotly and removed long/lat sliders, added inat observation year filter, added inat observation table

--- app.R
+++ app.R
@@ -2,7 +2,11 @@ library(shiny)
 library(bslib)
 library(osmdata)
 library(tidyverse)
+library(ggplot2); theme_set(theme_bw()); theme_update(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 library(rinat)
+library(plotly)
+library(lubridate)
+library(DT)
 
 # Define UI
 ui <- page_sidebar(
@@ -21,25 +25,23 @@ ui <- page_sidebar(
             "enter",
             label = "Find snails near me"
         ),
-        # User can adjust latitude and longitude within the bounds of their location
-        uiOutput("longControl"),
-        uiOutput("latControl")
+        # User can filter inaturalist observation dates
+         uiOutput("yearControl")
     ),
 
     # Inaturalist and paleobio db output
-        # Card for iNaturalist output
-    card(
-        "Snails near you",
-        layout_columns(
-            # Plots
-            card(
-                plotOutput("inat_map")
-            ),
-            # Table
-            card(
-                "placeholder"
+    # Card for iNaturalist output
+    navset_card_underline(
+        title = "Snails near you",
+        nav_panel("Map", plotlyOutput("inat_map")),
+        nav_panel("Abundance data", 
+            layout_columns(
+                plotlyOutput("inat_bar"), 
+                dataTableOutput("inat_abd")
             )
-        )
+        ),
+        nav_panel("All observations", dataTableOutput("inat_table")),
+        nav_panel("", tags$img(src='willem-dafoe-gq-style3.png', alt = "Willem Dafoe is delighted by his fancy coat", align = "center"))
     ),
     # Card for pbdb output
     card(
@@ -89,23 +91,15 @@ server <- function(input, output, session){
     # REACTIVE UI #
     ###############
 
-    ## Create sliders for adjusting latitude/longitude
-    output$longControl <- renderUI({
+    output$yearControl <- renderUI({
+        min_yr <- year(min(inat_data()$observed_on))
+        max_yr <- year(max(inat_data()$observed_on))
       sliderInput(
-            "longitude",
-            label = "Adjust longitude",
-            min = bb()[1,1],
-            max = bb()[1,2],
-            value = c(bb()[1,])
-        )
-    })
-    output$latControl <- renderUI({
-      sliderInput(
-            "latitude",
-            label = "Adjust latitude",
-            min = bb()[2,1],
-            max = bb()[2,2],
-            value = c(bb()[2,])
+            "year",
+            label = "Filter iNaturalist observations by year",
+            min = min_yr,
+            max = max_yr,
+            value = c(min_yr, max_yr)
         )
     })
 
@@ -113,23 +107,59 @@ server <- function(input, output, session){
     # INATURALIST OUTPUT #
     ######################
 
-    # Make iNaturalist map/plots
-    output$inat_map <- renderPlot({
+    # Make iNaturalist map
+    output$inat_map <- renderPlotly({
+        inat_data() %>%
+        # filter by year
+        filter(year(observed_on) >= min(input$year), year(observed_on) <= max(input$year)) %>%
+        # plot
         ggplot()+
-            geom_sf(data = map_feat()$osm_lines)+
-            theme_bw()+
-            ### * COME BACK AND FIX LIMITS * ##
-            xlim(input$longitude)+
-            ylim(input$latitude) +
             geom_point(
-                data = inat_data(), 
                 aes(x = longitude, y = latitude, color = scientific_name),
                 show.legend = F
-            )
+            )+
+            geom_sf(data = map_feat()$osm_lines)+
+            xlim(bb()[c(1,3)])+
+            ylim(bb()[c(2,4)]) +
+            theme(legend.position = "none")
     })
 
-    # Make iNaturalist table
-    ##PLACEHOLDER##
+    # Make iNaturalist abundance bar graph
+    output$inat_bar <- renderPlotly({
+        inat_data() %>%
+        # filter by year
+        filter(year(observed_on) >= min(input$year), year(observed_on) <= max(input$year)) %>% 
+        # Get genus variable
+        separate(scientific_name, into = c("genus","species"), sep = " ", remove = F) %>%
+        add_count(genus) %>%
+        # Order genus by abundance
+        mutate(genus = fct_reorder(genus, -n)) %>%
+        # Plot
+        ggplot(aes(x = genus, fill = scientific_name))+
+        geom_bar() +
+        theme(
+            legend.position = "none",
+            axis.text.x = element_text(angle = 60, hjust = 1)
+        )
+    })
+
+    # Make iNaturalist abundance data table
+    output$inat_abd <- renderDataTable({
+        inat_data() %>%
+        # filter by year
+        filter(year(observed_on) >= min(input$year), year(observed_on) <= max(input$year)) %>%
+        add_count(scientific_name) %>%
+        distinct(scientific_name, common_name, n)
+    })
+
+    # Make iNaturalist observation data table
+    output$inat_table <- renderDataTable({
+        inat_data() %>%
+        # filter by year
+        filter(year(observed_on) >= min(input$year), year(observed_on) <= max(input$year)) %>%
+        # don't display columns that include iNaturalist username or redundant info
+        select(scientific_name, place_guess:longitude, common_name, observed_on)
+    })
     
     ###############
     # PBDB OUTPUT #
